d7a66d1b14944ee30cf2ba778a4be8ab9f71dd47
Lennard <lennard@blank-hurst.de>
2023-06-09 00:11:06 +0200

    first try at getting and sending Data

--- Testing/test01.py
+++ Testing/test01.py
@@ -39,24 +39,104 @@ def loop():
 
     speed = ir['Speed']
     print('Speed:', speed)
+
+                      
+    pitch = ir['Pitch']
+    print('Pitch:', pitch)
+    PitchRate = ir['PitchRate'] #8.786555127926476e-08
+    #print('PitchRate:', PitchRate)
+    PitchRate_ST = ir['PitchRate_ST'] #[3.257972593928571e-07, 3.3143493283205316e-07, 3.4653390912353643e-07, 2.409470312159101e-07, 1.5829981236947788e-07, 8.786555127926476e-08]
+    #print('PitchRate_ST:', PitchRate_ST)
+
+    Roll = ir['Roll']
+    print('Roll:', Roll)
+    RollRate = ir['RollRate'] #3.12117554130964e-05
+    #print('RollRate:', RollRate)
+    RollRate_ST = ir['RollRate_ST'] #[-3.5479733924148604e-05, -3.106678923359141e-05, -2.1410816771094687e-05, -6.255076186789665e-06, 1.1896221622009762e-05, 3.12117554130964e-05]
+    #print('RollRate_ST:', RollRate_ST)
+
+    VertAccel = ir['VertAccel'] # 9.806662559509277
+    print('VertAccel:', VertAccel)
+
+    VertAccel_ST = ir['VertAccel_ST'] # [9.806638717651367, 9.806638717651367, 9.806644439697266, 9.806645393371582, 9.806645393371582, 9.806646347045898]
+    #print('VertAccel_ST:', VertAccel_ST)
+
+    Yaw = ir['Yaw']
+    print('Yaw:', Yaw)
     
     if chairConnected:
-        sendData()
+        move()
         print('Data send to chair')
 
 
-def sendData():
-    UDP_IP = "192.168.235.1"
-    UDP_PORT = 5005
-    MESSAGE = b"Test01 to %s" % UDP_IP
-    print("UDP target IP: %s" % UDP_IP)
-    print("UDP target port: %s" % UDP_PORT)
-    print("message: %s" % MESSAGE)
-    sock = socket.socket(socket.AF_INET, # Internet
-                          socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+#def sendData():
+#    UDP_IP = "192.168.235.1"
+#    UDP_PORT = 5005
+#    MESSAGE = b"Test01 to %s" % UDP_IP
+#    print("UDP target IP: %s" % UDP_IP)
+#    print("UDP target port: %s" % UDP_PORT)
+#    print("message: %s" % MESSAGE)
+#    sock = socket.socket(socket.AF_INET, # Internet
+#                          socket.SOCK_DGRAM) # UDP
+#    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
+def sendData(PitchAxis, RollAxis, HeaveAxis, YawAxis, LateralAxis, LongAxis, port):
+    host = "192.168.0.10"  # Replace with your desired IP address
+    endpoint = (host, port)
+    udpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    str = ":"
+    Faultys = ""
 
+    if len(PitchAxis) == 4:
+        if len(RollAxis) == 4:
+            if len(HeaveAxis) == 4:
+                if len(YawAxis) == 4:
+                    if len(LateralAxis) == 4:
+                        if len(LongAxis) == 4:
+                            try:
+                                s = "d" + PitchAxis + str + RollAxis + str + HeaveAxis + str + YawAxis + str + LateralAxis + str + LongAxis + "e"
+                                if s != "":
+                                    bytes = s.encode('utf-8')
+                                    udpClient.sendto(bytes, endpoint)
+                                elif len(IncommingNews) > 0:
+                                    bytes = str(IncommingNews[0]).encode('utf-8')
+                                    udpClient.sendto(bytes, endpoint)
+                            except Exception as ex:
+                                Faultys.append(ex)
+                        else:
+                            Faultys.append("Longaxis.text is too long or too short")
+                    else:
+                        Faultys.append("Lateralaxis.text is too long or too short")
+                else:
+                    Faultys.append("Yawaxis.text is too long or too short")
+            else:
+                Faultys.append("Heaveaxis.text is too long or too short")
+        else:
+            Faultys.append("Rollaxis.text is too long or too short")
+    else:
+        Faultys.append("Pitchaxis.text is too long or too short")
+
+def move(_position, _rotation):
+    # convert vectors to string-components and send to X6
+    xPos = toHexapodString(-_position.x)
+    yPos = toHexapodString(_position.y)
+    zPos = toHexapodString(-_position.z)
+    xRot = toHexapodString(_rotation.x)
+    yRot = toHexapodString(-_rotation.y)
+    zRot = toHexapodString(_rotation.z)
+    # X6.SendData(xRot, zRot, yPos, yRot, xPos, zPos, 59999)  # default values
+    # X6.SendData(zPos, zRot, yPos, xPos, yRot, xRot, 59999)  # switched values
+    # X6.SendData(xRot, zRot, yRot, yPos, xPos, zPos, 59999)  # default values
+    sendData(xRot, zRot, yPos, yRot, xPos, zPos, 59999)  # default values
+    # X6.SendData("0", "0", "0", xPos, yPos, zPos, 59999)  # default values
+    debugText.text = "Pos: " + str(_position) + ", rot" + str(_rotation)
+    # Debug.Log("zPos: "+zPos + ", zRot: " + zRot + ",yPos: " + yPos + ",xPos" + xPos + ",yRot: " + yRot + ",xRot: " + xRot)
+
+# create a three-digit string with leading 1 (-) or 0 (+) according to the sign
+def toHexapodString(_f):
+    res = "1" if _f < 0 else "0"
+    res += str(abs(_f)).zfill(3)
+    return res
 
 if __name__ == '__main__':
     # initializing ir and state
@@ -74,7 +154,7 @@ if __name__ == '__main__':
             # sleep for 1 second
             # maximum you can use is 1/60
             # cause iracing updates data with 60 fps
-            time.sleep(1)
+            time.sleep(1/60)
     except KeyboardInterrupt:
         # press ctrl+c to exit
         pass
---
+++ X6/X6Motion.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace X6Motion
+{
+    public class MainMotion
+    {
+        private ArrayList IncommingNews = new ArrayList();
+        private ArrayList Faultys = new ArrayList();
+        private ArrayList MySerialPorts = new ArrayList();
+        private string MyEndPoint;
+        private string MySelfPoint;
+        private string anyIPChoosed;
+        private int MyLocalPort;
+        private string MyIncommingByte;
+        private string MyFaultys;
+        private string MyComPort;
+        private string existingSerialPort;
+        private static SerialPort _serialPort;
+
+        public int GetIncommingDataCount
+        {
+            get
+            {
+                return this.IncommingNews.Count;
+            }
+        }
+
+        public int GetFaultysCount
+        {
+            get
+            {
+                return this.Faultys.Count;
+            }
+        }
+
+        public string GetFaultyError
+        {
+            get
+            {
+                return this.MyFaultys;
+            }
+        }
+
+        public string GetIncommingNews
+        {
+            get
+            {
+                return this.MyIncommingByte;
+            }
+        }
+
+        public int GetMyExistingSerialPortsCount
+        {
+            get
+            {
+                return this.MySerialPorts.Count;
+            }
+        }
+
+        public string GetSerialName
+        {
+            get
+            {
+                return this.existingSerialPort;
+            }
+        }
+
+        public string SetComPort
+        {
+            set
+            {
+                this.MyComPort = value;
+            }
+        }
+
+        public void EndPoint(string EndPoint)
+        {
+            this.MyEndPoint = EndPoint;
+        }
+
+        public void SelfPoint(string SelfPoint)
+        {
+            this.MySelfPoint = SelfPoint;
+        }
+
+        public void IPListening(string IpListening)
+        {
+            this.anyIPChoosed = IpListening;
+        }
+
+        public void localPort(int yourLocalPort)
+        {
+            this.MyLocalPort = yourLocalPort;
+        }
+
+        public void IncommingNewsWithNetwork()
+        {
+            new Thread(new ThreadStart(this.ReceiveDataFromNetwork))
+            {
+                IsBackground = true
+            }.Start();
+        }
+
+        private void ReceiveDataFromNetwork()
+        {
+            UdpClient udpClient = new UdpClient(this.MyLocalPort);
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, this.MyLocalPort);
+                    this.IncommingNews.Add((object)Encoding.UTF8.GetString(udpClient.Receive(ref remoteEP)));
+                }
+                catch (Exception ex)
+                {
+                    this.Faultys.Add((object)ex);
+                }
+            }
+        }
+
+        public void ReceiveIncommingNews(int i)
+        {
+            this.MyIncommingByte = Convert.ToString(this.IncommingNews[i]);
+        }
+
+        public void SetIncommingNews(int i, string data)
+        {
+            this.IncommingNews[i] = (object)data;
+        }
+
+        public void ClearIncommingNews()
+        {
+            this.IncommingNews.Clear();
+        }
+
+        public void ReceiveFaultysNews(int i)
+        {
+            this.MyFaultys = Convert.ToString(this.Faultys[i]);
+        }
+
+        public void ClearFaultys()
+        {
+            this.Faultys.Clear();
+        }
+
+        public void SendData(string PitchAxis, string RollAxis, string HeaveAxis, string YawAxis, string LateralAxis, string LongAxis, int port)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(this.MyEndPoint), port);
+            UdpClient udpClient = new UdpClient();
+            string str = ":";
+            if (PitchAxis.Length == 4)
+            {
+                if (RollAxis.Length == 4)
+                {
+                    if (HeaveAxis.Length == 4)
+                    {
+                        if (YawAxis.Length == 4)
+                        {
+                            if (LateralAxis.Length == 4)
+                            {
+                                if (LongAxis.Length == 4)
+                                {
+                                    try
+                                    {
+                                        string s = "d" + PitchAxis + str + RollAxis + str + HeaveAxis + str + YawAxis + str + LateralAxis + str + LongAxis + "e";
+                                        if (s != "")
+                                        {
+                                            byte[] bytes = Encoding.UTF8.GetBytes(s);
+                                            udpClient.Send(bytes, bytes.Length, endPoint);
+                                        }
+                                        else if (this.IncommingNews.Count > 0)
+                                        {
+                                            byte[] bytes = Encoding.UTF8.GetBytes(Convert.ToString(this.IncommingNews[0]));
+                                            udpClient.Send(bytes, bytes.Length, endPoint);
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        this.Faultys.Add((object)ex);
+                                    }
+                                }
+                                else
+                                    this.Faultys.Add((object)"Longaxis.text is too long or too short");
+                            }
+                            else
+                                this.Faultys.Add((object)"Lateralaxis.text is too long or too short");
+                        }
+                        else
+                            this.Faultys.Add((object)"Yawaxis.text is too long or too short");
+                    }
+                    else
+                        this.Faultys.Add((object)"Heaveaxis.text is too long or too short");
+                }
+                else
+                    this.Faultys.Add((object)"Rollaxis.text is too long or too short");
+            }
+            else
+                this.Faultys.Add((object)"Pitchaxis.text is too long or too short");
+        }
+
+        public void GetExistingSerialPorts()
+        {
+            this.MySerialPorts.Clear();
+            foreach (object obj in SerialPort.GetPortNames())
+                this.MySerialPorts.Add(obj);
+        }
+
+        public void GetExistingSerialNames(int i)
+        {
+            this.existingSerialPort = Convert.ToString(this.MySerialPorts[i]);
+        }
+
+        public void InitializeSerialPort(string SerialPortName, int SerialTimeOut)
+        {
+            MainMotion._serialPort = new SerialPort(SerialPortName, 59999, Parity.None, 8, StopBits.One);
+            MainMotion._serialPort.WriteTimeout = SerialTimeOut;
+            MainMotion._serialPort.Open();
+        }
+
+        public void SendDataWithSerialPort(string PitchAxis, string RollAxis, string HeaveAxis, string YawAxis, string LateralAxis, string LongAxis, int port)
+        {
+            string str = ":";
+            if (PitchAxis.Length == 4)
+            {
+                if (RollAxis.Length == 4)
+                {
+                    if (HeaveAxis.Length == 4)
+                    {
+                        if (YawAxis.Length == 4)
+                        {
+                            if (LateralAxis.Length == 4)
+                            {
+                                if (LongAxis.Length == 4)
+                                {
+                                    try
+                                    {
+                                        string text = "d" + PitchAxis + str + RollAxis + str + HeaveAxis + str + YawAxis + str + LateralAxis + str + LongAxis + "e";
+                                        if (!(text != ""))
+                                            return;
+                                        MainMotion._serialPort.Write(text);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        this.Faultys.Add((object)ex);
+                                    }
+                                }
+                                else
+                                    this.Faultys.Add((object)"Longaxis.text is too long or too short");
+                            }
+                            else
+                                this.Faultys.Add((object)"Lateralaxis.text is too long or too short");
+                        }
+                        else
+                            this.Faultys.Add((object)"Yawaxis.text is too long or too short");
+                    }
+                    else
+                        this.Faultys.Add((object)"Heaveaxis.text is too long or too short");
+                }
+                else
+                    this.Faultys.Add((object)"Rollaxis.text is too long or too short");
+            }
+            else
+                this.Faultys.Add((object)"Pitchaxis.text is too long or too short");
+        }
+    }
+}
